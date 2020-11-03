414b583c799c7944164b8eb3cfa845ae2600ae2a
Cesar Semsey <ce.semsey@gmail.com>
2020-11-03 14:20:19 -0300

    change pi

--- js/api.js
+++ js/api.js
@@ -1,5 +1,5 @@
-var BASE_URL = "http://127.0.0.1:8000/api";
-// var BASE_URL = "https://pss2020api.herokuapp.com/api";
+// var BASE_URL = "http://127.0.0.1:8000/api";
+var BASE_URL = "https://pss2020api.herokuapp.com/api";
 var api = {
     auth:{
         login: BASE_URL + "/login",
