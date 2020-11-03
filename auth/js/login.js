ff6083eabb910c93794124228c51f00b1ea90846
Guillermo <guilleseewald@gmail.com>
2020-11-03 17:34:32 -0300

    tratando de redirigir

--- auth/js/login.js
+++ auth/js/login.js
@@ -1,3 +1,19 @@
+function start(auth) {
+    switch (auth) {
+        case 'admin':
+            window.location.href = '../signatures/asociar.hmtl';
+            break;
+        case 'docente':
+            window.location.href = '../docente/notas/notas.hmtl';
+            break;
+        case 'alumno':
+            window.location.href = '../alumno/inscripcion/career.hmtl';
+            break;
+        default:
+
+    }
+}
+
 window.onload = function(){
 
     let btnIngresar = document.getElementById('btn-ingresar');
@@ -9,10 +25,13 @@ window.onload = function(){
             if(response.status == 200){
                 localStorage.setItem('access_token', response.data.access_token);
                 setTimeout(function(){
-                    window.location.href = '../signatures/asociar.html';
+                    checkToken(['admin', 'docente', 'alumno'], start);
                 },100)
             }
-        })
+        }).catch(
+            (e) => {
+                alert("Error al loguearse");
+            }
+        )
     })
-
 }
